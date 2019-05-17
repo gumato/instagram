89214061c64cbf6fa8621e4c0310ea5d5783b3e6
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-17 08:06:30 +0300

    Create folder structure

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pricilla/Documents/moringa-school-projects/instagram/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
