36e89937e28b08d2c03dafa1e5f20dff65311aeb
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-18 12:56:45 +0300

    Create form file for creating form class

--- gram/views.py
+++ gram/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request,'')
