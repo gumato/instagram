a8eee77d0d942f8356337babfd82c408b98e37d9
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-19 09:24:01 +0300

    Adding class form to page

--- gram/forms.py
+++ gram/forms.py
@@ -2,5 +2,22 @@ from django import forms
 from .models import Image,Profile, Comments
 
 class ImageForm(forms.ModelForm):
-    your_name = forms.CharField(label='First Name',max_length=30)
-    email = forms.EmailField(label='Email')
+    class Meta:
+        model = Image
+        exclude = ['likes','profile','posted_time','profile','user']
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['image','user']
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','profile_pic']
--- gram/views.py
+++ gram/views.py
@@ -61,3 +61,51 @@ def new_image(request):
     else:
         form = ImageForm()
     return render(request, 'new_image.html', {"form": form})
+
+
+def profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    print(profile.profile_pic)
+    posts = Image.objects.filter(user=current_user)
+    if request.method == 'POST':
+        signup_form = EditForm(request.POST, request.FILES,instance=request.user.profile) 
+        if signup_form.is_valid():
+           signup_form.save()
+    else:        
+        signup_form =EditForm() 
+    
+    return render(request, 'registration/profile.html', {"date": date, "form":signup_form,"profile":profile, "posts":posts})
+
+@login_required(login_url='/accounts/login/')
+def comment(request,image_id):
+    #Getting comment form data
+    if request.method == 'POST':
+        image = get_object_or_404(Image, pk = image_id)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.image = image
+            comment.save()
+    return redirect('index')
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_users = User.objects.filter(username=search_term)
+        message = f"{search_term}"
+        profiles=  Profile.objects.all( )
+        
+        return render(request, 'all-posts/search.html',{"message":message,"users": searched_users,'profiles':profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-posts/search.html',{"message":message})
+        
+def profiles(request,id):
+    profile = Profile.objects.get(user_id=id)
+    post=Image.objects.filter(user_id=id)
+                       
+    return render(request,'profiles_each.html',{"profile":profile,"post":post})       
