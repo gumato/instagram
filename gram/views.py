730418e367e22b7c0e0f5628cd0fe8fe4af11830
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-19 08:51:04 +0300

    Creating sign function

--- gram/views.py
+++ gram/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 from .forms import ImageForm
 
 # Create your views here.
-def welcome(request):
-    return render(request,'')
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your Instagram account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
